5b0ce08785a04dbc63bb077b6822a79e6d794d09
Just-Prog <JustProg5835@outlook.com>
2025-04-18 17:20:19 +0800

    axios拦截器报错优化 调了半天不弹框发现是antd5没适配react19 啊卧槽react19怎么这么坏.jpg

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,17 +1,34 @@
-import Axios from "axios";
+import { message } from "antd";
+import axios from "axios";
 
-const api = Axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_API_URL,
 });
 
-api.interceptors.request.use((config) => {
-  config.headers["X-AppVer"] = "pc:teaching:pc:3.0-1";
-  return config;
-});
+api.interceptors.request.use(
+  config => {
+    config.headers["X-AppVer"] = "pc:teaching:pc:3.0-1";
+    return config;
+  },
+  error => {
+    message.error(error.code);
+    return Promise.reject(error);
+  }
+);
 
-api.interceptors.response.use((config) => {
-  console.log(config);
-  return config;
-});
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  (error) => {
+    message.error(
+      error.response.data.message ||
+        error.response.statusText ||
+        error.message ||
+        error.code
+    );
+    return Promise.reject(error);
+  }
+);
 
 export default api;
