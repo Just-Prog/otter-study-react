0932e3b38bafcc0c6d338357cf8ec1dcd1c3dc15
Just-Prog <JustProg5835@outlook.com>
2025-04-19 23:45:14 +0800

    优化主页面响应式布局 现在在手机尺寸下可通过顶栏按钮调用抽屉使用原顶栏右侧的功能

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import {Avatar, Layout, Menu, Dropdown, Button, Row, Col, Carousel} from "antd";
-const { Header, Content, Footer } = Layout;
+import {Avatar, Layout, Menu, Dropdown, Button, Row, Col, Carousel, Drawer} from "antd";
+const { Header, Content, Footer, Sider } = Layout;
 import { useNavigate } from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 
@@ -11,8 +10,10 @@ import default_avatar from "@/assets/avatar/default.svg";
 import logo from "@/assets/logo.png";
 import {UserLoginCard} from "@/pages/user/login.jsx";
 import { logout } from '@/stores/user.jsx';
+import {MenuOutlined} from "@ant-design/icons";
+import {useState} from "react";
 
-function NavBarRight() {
+function NavBarRight({ inDrawer = false }) {
   const isLogined = useSelector(state => state.user.isLogined);
   const dispatch = useDispatch();
   const nav = useNavigate();
@@ -37,75 +38,108 @@ function NavBarRight() {
     items: dropdownItems,
   };
   if (isLogined)
-    return (
+    if(inDrawer)
+      return (
+        <div style={{width:'100%',display:'flex',flexDirection:'column',alignItems:'center'}}>
+          <Avatar src={default_avatar} size={64}></Avatar>
+          <div style={{height: '30px'}}/>
+          <Menu items={dropdownItems} style={{width: '100%'}}/>
+        </div>
+      );
+    else return (
       <Dropdown
-        trigger={["click", "hover"]}
-        placement="bottom"
-        arrow={true}
-        menu={dropdownMenuProps}
-        onClick={handleClick}
+          trigger={["click", "hover"]}
+          placement="bottom"
+          arrow={true}
+          menu={dropdownMenuProps}
+          onClick={handleClick}
       >
         <Avatar src={default_avatar}></Avatar>
       </Dropdown>
     );
   else
     return (
-      <Button color="default" type="primary" onClick={() => onButtonClicked()}>
-        登录/注册
-      </Button>
+      <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:'100%',height:'100%'}}>
+        <Button color="default" type="primary" onClick={() => onButtonClicked()}>
+          登录/注册
+        </Button>
+      </div>
     );
 }
 
-function IndexPage() {
-  const [current, setCurrent] = useState("/");
-  const onClick = (e) => {
-    setCurrent(e.key);
-  };
-  const navItems = [
-    {
-      key: "/",
-      label: "首页",
-    },
-  ];
-
+function IndexFrame({children}) {
+  const isLogined = useSelector(state => state.user.isLogined);
+  const [open, setOpen] = useState(false);
+  const openDrawer = () => {
+    setOpen(true);
+  }
+  const closeDrawer = () => {
+    setOpen(false);
+  }
   return (
-    <Layout>
-      <Header id="layoutHeader">
-        <div id="layoutHeaderMain">
-          <div className="logo">
-            <img src={logo} height="52" />
-          </div>
-          <div id="layoutHeaderMenu" style={{ flex: 1 }}>
-            <Menu
-              mode="horizontal"
-              items={navItems}
-              defaultSelectedKeys={[current]}
-              onClick={onClick}
-            />
-          </div>
-          <div id="layoutAvatar">
-            <NavBarRight />
-          </div>
-        </div>
-      </Header>
-      <Content id="layoutContent">
-        <div id="layoutContentMiddle">
-          <Row>
-            <Col xs={24} lg={12}>
-              {/*<Carousel/>*/}111
-            </Col>
-            <Col xs={0} lg={12}>
-              <UserLoginCard style={{background: "linear-gradient(180deg, rgba(55, 120, 252, 0.15), white 60%)", width: "100%"}}/>
-            </Col>
-          </Row>
-        </div>
-      </Content>
-      <Footer id="layoutFooter">
-        <div id="layoutFooterMain">
-          <span>OtterStudy {new Date().getFullYear()}</span>
-        </div>
-      </Footer>
-    </Layout>
+      <Layout>
+        { isLogined && <Sider id="layoutSider" breakpoint={'lg'} style={{background: "#fff"}} width="240px"></Sider> }
+        <Layout id="layoutMain">
+          <Header id="layoutHeader">
+            <div id="layoutDrawerTrigger">
+              <Button icon={<MenuOutlined />} onClick={()=>openDrawer()}/>
+            </div>
+            <div id="layoutHeaderMain">
+              <div className="logo">
+                <img src={logo} height="52" />
+              </div>
+              <div id="layoutAvatar">
+                <NavBarRight />
+              </div>
+            </div>
+          </Header>
+          <Content id="layoutContent">
+            <div id="layoutContentMiddle">
+              {children}
+            </div>
+          </Content>
+          <Footer id="layoutFooter">
+            <div id="layoutFooterMain">
+              <span>OtterStudy {new Date().getFullYear()}</span>
+            </div>
+          </Footer>
+        </Layout>
+        <Drawer
+            placement="left"
+            closable={false}
+            onClose={closeDrawer}
+            open={open}
+            width={315}
+        >
+          <NavBarRight inDrawer={true}/>
+        </Drawer>
+      </Layout>
+  );
+}
+
+function IndexPage() {
+  const isLogined = useSelector(state => state.user.isLogined);
+  if (!isLogined)
+    return (
+        <>
+          <IndexFrame>
+            <Row>
+              <Col xs={24} lg={12}>
+                111
+              </Col>
+              <Col xs={0} lg={12}>
+                <UserLoginCard style={{background: "linear-gradient(180deg, rgba(55, 120, 252, 0.15), white 60%)", width: "100%"}}/>
+              </Col>
+            </Row>
+          </IndexFrame>
+        </>
+    );
+  else return (
+      <>
+        <IndexFrame>
+          constructing
+        </IndexFrame>
+      </>
   );
 }
 
