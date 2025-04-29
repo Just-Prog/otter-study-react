2fd242b7a3439234c50ae39be70985b7106431c2
Just-Prog <JustProg5835@outlook.com>
2025-04-29 14:30:38 +0800

    学习页课程展示与操作(课程操作/归档操作)功能完善 // TODO 归档管理列表分页

--- src/pages/index/learning/learning.jsx
+++ src/pages/index/learning/learning.jsx
@@ -1,5 +1,7 @@
 import { IndexFrame } from "@/pages/index/index.jsx";
-import {Button, Card, Col, Empty, Popover, Row, Space, Tabs} from "antd";
+import {Button, Card, Col, Drawer, Empty, message, Modal, Popover, Row, Space, Spin, Tabs, Input} from "antd";
+import {BookOutlined, CloseCircleOutlined, EyeOutlined, SearchOutlined, ToTopOutlined} from "@ant-design/icons";
+
 import {useEffect, useState} from "react";
 
 import api from "@/api/api.jsx";
@@ -7,38 +9,97 @@ import api from "@/api/api.jsx";
 import "./learning.css"
 
 import default_cropper from "@/assets/default-cropper.png"
-import {BookOutlined, CloseCircleOutlined, ToTopOutlined} from "@ant-design/icons";
 
 const ClassBigCard = ({children}) => {
     return (
         <>
             <Card className="class_tiles">
-                {children || <Empty style={{minHeight: "100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}/>}
+                {children || <Spin/>}
             </Card>
         </>
     )
 }
 
 const ClassTilesPage = () => {
+    const [messageApi, contextHolder] = message.useMessage();
     const [classes, setClasses] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
+    const [archiveVisible, setArchiveVisible] = useState(false);
+    const [addDialogVisible, setAddDialogVisible] = useState(false);
+    const [archiveList, setArchiveList] = useState([]);
     const fetchClassList = async () => {
         let resp = await api.post("/tac/class/v1/stu/class",{
             name: searchQuery
         });
         setClasses(resp.data);
     }
+
+    const changeTopStatus = async (item) => {
+        let resp = await api.put(`/tac/class/top/${item.id}`);
+        await fetchClassList();
+        messageApi.success(resp.data.message);
+    }
+    const changeArchiveStatus = async (item) => {
+        let resp = await api.post("/tac/class/archives",{
+            classId: item.id,
+        });
+        await fetchClassList();
+        messageApi.success(resp.data.message);
+    }
+    const quitClass = async (item) => {
+        let resp = await api.post("/tac/class/exitClass",{
+            classId: item.id,
+            courseId: item.courseId
+        });
+        await fetchClassList();
+        messageApi.success(resp.data.message);
+    }
+
+    const openArchiveDrawer = ()=>{
+        setArchiveVisible(true);
+    }
+    const closeArchiveDrawer = () => {
+        fetchClassList();
+        setArchiveVisible(false);
+    }
+    const openAddDialog = async()=>{
+        setAddDialogVisible(true);
+    }
     useEffect(() => {
         fetchClassList();
     },[searchQuery])
     return (
         <>
+            {contextHolder}
             <ClassBigCard>
+                <div style={{display: "flex", justifyContent: "flex-end"}}>
+                    <Space>
+                        { /* TODO 分页 */}
+                        <Input.Search
+                            placeholder="搜索班课名/任课教师"
+                            allowClear
+                            enterButton={<SearchOutlined/>}
+                            onSearch={(v)=>setSearchQuery(v)}
+                        />
+                        <Button onClick={() => openArchiveDrawer()}>
+                            <span>
+                                归档管理
+                            </span>
+                        </Button>
+                        <Button type="primary" onClick={() => openAddDialog()}>
+                            <span>
+                                加入班课
+                            </span>
+                        </Button>
+                    </Space>
+                </div>
                 {classes.map((item, index) => <Card title={item.category}>
                     <Row gutter={[15, 15]}>
                         {item.classResList.map(
                             (i, index) => <Col xs={12} lg={8}>
                                 <Card
+                                    hoverable
+                                    className="class_tiles_item"
                                     cover={
                                         <img
                                             src={i.imageUrl.includes("default-cropper") ? default_cropper : i.imageUrl}
@@ -50,14 +111,13 @@ const ClassTilesPage = () => {
                                     }
                                     actions={[
                                         <Popover content={"置顶"}>
-                                            <ToTopOutlined />
+                                            <ToTopOutlined onClick={() => changeTopStatus(i)}/>
                                         </Popover>,
-                                        //TODO 取消置顶
                                         <Popover content={"归档"}>
-                                            <BookOutlined />
+                                            <BookOutlined onClick={() => changeArchiveStatus(i)}/>
                                         </Popover>,
                                         <Popover content={"退课"}>
-                                            <CloseCircleOutlined />
+                                            <CloseCircleOutlined onClick={() => quitClass(i)}/>
                                         </Popover>,
                                     ]}
                                 >
@@ -74,28 +134,89 @@ const ClassTilesPage = () => {
                     </Row>
                 </Card>)}
             </ClassBigCard>
+            <Drawer title="归档管理" open={archiveVisible} onClose={() => closeArchiveDrawer()}
+                    classNames="classes_archives">
+                {archiveVisible ? <ArchiveManagePage/> : null}
+            </Drawer>
+            <Modal title="添加班课" open={addDialogVisible} onCancel={() => setAddDialogVisible(false)} onOk={() => {
+                // TODO 确定逻辑
+                setAddDialogVisible(false);
+            }}>
+                // TODO
+            </Modal>
         </>
     );
 }
 
-const ClassOperations = () => {
+const ArchiveManagePage = () => {
+    const [messageApi, contextHolder] = message.useMessage();
+    const [searchQuery, setSearchQuery] = useState("");
+    const [archiveList, setArchiveList] = useState([]);
+    const changeArchiveStatus = async (item) => {
+        let resp = await api.post("/tac/class/archives",{
+            classId: item.id,
+        });
+        messageApi.success("取消归档成功"); // 后端归档和取消归档都返回"归档成功"，难绷
+    }
+    const fetchArchiveList = async () => {
+        let resp = await api.post("/tac/class/getArchivesClass",{
+            classType: "", // 0普通班课 1公开课
+            pageNum: 1, //todo 翻页
+            pageSize: 100,
+            query: "" //todo 搜索
+        });
+        setArchiveList(resp.data.list);
+    }
+    useEffect(() => {
+        fetchArchiveList()
+    }, [searchQuery]);
     return (
         <>
-            <Space>
-                <Button>
-                    <span>
-                        归档管理
-                    </span>
-                </Button>
-                <Button type="primary">
-                    <span>
-                        加入班课
-                    </span>
-                </Button>
-            </Space>
+            {contextHolder}
+            {archiveList.length !== 0 ?
+                <div className="archive_manage_list">
+                    <div className="archive_manage_header">
+                        <Input.Search
+                            placeholder="搜索班课名/任课教师"
+                            allowClear
+                            enterButton={<SearchOutlined/>}
+                            onSearch={(v)=>setSearchQuery(v)}
+                        />
+                    </div>
+                    <Row gutter={[15, 15]}>
+                        {archiveList.map((item, index) => {
+                            return (
+                                <Col span={24}>
+                                    <Card
+                                        hoverable
+                                        actions={[
+                                            <Popover content={"归档"}>
+                                                <BookOutlined onClick={() => changeArchiveStatus(item)}/>
+                                            </Popover>,
+                                            <Popover content={"查看"}>
+                                                <EyeOutlined onClick={() => {}}/>
+                                            </Popover>
+                                        ]}
+                                    >
+                                        <Card.Meta
+                                            title={item.className}
+                                            description={<div>
+                                                任课教师: {item.creator}<br/>
+                                                类别: {item.type === 0 ? "普通班课" : "公开课"}
+                                            </div>}
+                                        />
+                                    </Card>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </div>
+                : <Empty/>
+            }
         </>
     );
 }
+
 const LearningIndexPage = () => {
     const items = [
         {
@@ -115,9 +236,8 @@ const LearningIndexPage = () => {
         <>
             <IndexFrame>
                 <Tabs
-                    defaultActiveKey="1"
+                    defaultActiveKey="classes"
                     items={items}
-                    tabBarExtraContent={ClassOperations()}
                 />
             </IndexFrame>
         </>
