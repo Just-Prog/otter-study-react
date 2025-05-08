594026dc447bcb66c354563bdabff3902cefbba3
Just-Prog <JustProg5835@outlook.com>
2025-05-08 11:05:39 +0800

    课程详情页路由守护&加载

--- src/pages/class/class_detail.jsx
+++ src/pages/class/class_detail.jsx
@@ -1,8 +1,9 @@
 import {IndexFrame} from "@/pages/index/index.jsx";
-import {Outlet, useParams} from "react-router";
-import {Card, Space, Typography} from "antd";
+import {Outlet, useLocation, useMatch, useParams} from "react-router";
+import {Alert, Card, Space, Spin, Typography} from "antd";
 import {useEffect, useState} from "react";
 import api from "@/api/api.jsx";
+import {useNavigate} from "react-router-dom";
 
 const ClassCourseWarePage = ()=>{}
 const ClassInfoPage = ()=>{}
@@ -18,26 +19,45 @@ const ClassMainPage = () => {
         let resp = await api.post("/tac/class/classInfo",{classId: classId});
         setCourseData(resp.data);
     }
+    const [loading,setLoading] = useState(true);
+    const nav = useNavigate();
+    const location = useLocation();
+    const isRootRoute = useMatch({ path: '/class-detail/:classId/:courseId', end: true });
+    const isChildRoute = useMatch({ path: '/class-detail/:classId/:courseId/', end: false }) && !isRootRoute;
     useEffect(() => {
-        fetchClassInfo();
-    }, []);
+        if(isChildRoute){
+            fetchClassInfo().then(()=>{
+                setLoading(false);
+            });
+        }else{
+            nav(`/class-detail/${classId}/${courseId}/courseware`);
+            fetchClassInfo().then(()=>{
+                setLoading(false);
+            });
+        }
+    }, [location.pathname]);
     return (
         <IndexFrame>
-            <div style={{rowGap: "16px", width: "100%", display: "flex", flexDirection: "column"}}>
-                <Card style={{textOverflow: "ellipsis", overflowX: "auto"}}>
-                    <div>
-                        <div style={{fontWeight: "bold", fontSize: "18px"}}>
-                            {courseData.className ?? "Default"} ({courseData.classCode ?? "00AA00"})
+            {loading
+                ? <Spin size="large">
+                    <Alert message="加载中" type="info" />
+                </Spin>
+                : <div style={{rowGap: "16px", width: "100%", display: "flex", flexDirection: "column"}}>
+                    <Card style={{textOverflow: "ellipsis", overflowX: "auto"}}>
+                        <div>
+                            <div style={{fontWeight: "bold", fontSize: "18px"}}>
+                                {courseData.className ?? "Default"} ({courseData.classCode ?? "00AA00"})
+                            </div>
+                            <div style={{fontSize: "15px"}}>
+                                任课教师: {courseData.teacher ?? "..."}
+                            </div>
                         </div>
-                        <div style={{fontSize: "15px"}}>
-                            任课教师: {courseData.teacher ?? "..."}
-                        </div>
-                    </div>
-                </Card>
-                <Card style={{textOverflow: "ellipsis", overflowX: "auto"}}>
-                    <Outlet/>
-                </Card>
-            </div>
+                    </Card>
+                    <Card style={{textOverflow: "ellipsis", overflowX: "auto"}}>
+                        <Outlet/>
+                    </Card>
+                </div>
+            }
         </IndexFrame>
     )
 }
