903eba06d022454ca8c931b2692579ba64bb1aaa
Just-Prog <JustProg5835@outlook.com>
2025-06-04 12:54:29 +0800

    课程作业模块雏形（暂时没有提交和重做功能）

--- src/components/class/homework.jsx
+++ src/components/class/homework.jsx
@@ -1,18 +1,79 @@
-import {useParams} from "react-router";
-import {Card} from "antd";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import {Card, Col, List, Row, Space, Input} from "antd";
+const { TextArea } = Input;
+import api from "@/api/api";
+import FileItem from "@/components/common/file_item.jsx";
 
 const ClassHomeworkComponent = ()=>{
     const params = useParams();
+    const classId = params.classId;
     const dataId = params.actId;
+    const [data,setData] = useState({});
+    const formatter = new Intl.DateTimeFormat("zh-CN", {
+        timeZone: "Asia/Shanghai",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    })
+    const fetchHomeWorkDet = async()=>{
+        let resp = await api.post("/tac/homeworkStudent/getHomeWorkDet",{
+            homeworkId: dataId,
+            classId: classId,
+        });
+        setData(resp.data);
+    }
+    useEffect(()=>{
+        fetchHomeWorkDet();
+    },[dataId])
     return (
         <>
-            <Card>
-                {dataId}
-            </Card>
-
-            <Card style={{marginTop: 16}}>
-                {JSON.stringify({})}
-            </Card>
+            <Row gutter={[16,16]}>
+                <Col lg={12} xs={24}>
+                    <Card title={"作业内容"}>
+                        <Space direction={"vertical"} style={{display: "flex"}}>
+                            <Card style={{backgroundColor:"#eeeeee"}}>
+                                {data.introduction ?? ""}
+                            </Card>
+                            {data.homeworkDocListResList && data.homeworkDocListResList.length > 0
+                                ? <Card>
+                                    <List
+                                        dataSource={data.homeworkDocListResList}
+                                        renderItem={(item) => (
+                                            <FileItem filename={item.docName} ext={item.type} url={item.imgUrl} />
+                                        )}
+                                    />
+                                </Card>
+                                : <></>
+                            }
+                        </Space>
+                    </Card>
+                </Col>
+                <Col lg={12} xs={24}>
+                    <Card title={"交作业"}>
+                        <Space direction={"vertical"} style={{display: "flex"}}>
+                            <TextArea
+                                placeholder="请输入你的观点……"
+                                autoSize={{ minRows: 3, maxRows: 6 }}
+                            />
+                            {data.homeworkDocListResList && data.stuHomeworkDocListResList.length > 0
+                                ? <Card>
+                                    <List
+                                        dataSource={data.stuHomeworkDocListResList}
+                                        renderItem={(item) => (
+                                            <FileItem filename={item.docName} ext={item.type} url={item.imgUrl} />
+                                        )}
+                                    />
+                                </Card>
+                                : <></>
+                            }
+                        </Space>
+                    </Card>
+                </Col>
+            </Row>
         </>
     )
 }
---
+++ src/components/common/file_item.jsx
@@ -0,0 +1,33 @@
+import {fileExt2Icons} from "@/components/common/otter_common_define.js";
+import {CloudDownloadOutlined} from "@ant-design/icons";
+import api from "@/api/api.jsx";
+
+function FileItem({imgh=20, filename = "default.bin", ext = "bin", url = ""}){
+    return <>
+        <div style={{display: "flex", flexDirection: "row", height: imgh + 5, alignItems: "center", width:"100%",}}>
+            <img height={imgh} src={fileExt2Icons(ext)} />
+            <div style={{flex:1, paddingLeft: "5px", paddingRight: "5px", overflowX:"hidden",textOverflow:"ellipsis", whiteSpace:"nowrap",fontSize:imgh-7}}>
+                {filename}
+            </div>
+            <div>
+                <CloudDownloadOutlined onClick={async()=>{
+                    if(url.includes("obs.goktech.cn")){
+                        url = `/obs/${url.split("https://obs.goktech.cn/")[1]}`;
+                    }
+                    let resp = await api.get(url,{
+                        baseURL: null,
+                        responseType: 'blob'
+                    });
+                    const href = URL.createObjectURL(resp.data, { type: resp.headers['content-type'] });
+                    let link = document.createElement('a');
+                    link.download = filename;
+                    link.href = href;
+                    link.click();
+                    URL.revokeObjectURL(href);
+                }}/>
+            </div>
+        </div>
+    </>
+}
+
+export default FileItem;
